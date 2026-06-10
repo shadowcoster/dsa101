c976c1e3342a834107174881694f50bb555ef9d6
MayankMehra <mehramayank28@gmail.com>
2026-06-10 12:18:51 +0530

    contains dublicates

---
+++ FAANG-DSA-Roadmap/00-start-here/bruteforce/containsduplicate.js
@@ -0,0 +1,12 @@
+const containsDuplicate = (nums) => {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] === nums[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+const nums = [1, 2, 3, 1];
+console.log(containsDuplicate(nums)); //true
