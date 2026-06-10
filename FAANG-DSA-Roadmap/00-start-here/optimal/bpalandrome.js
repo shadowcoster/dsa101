064ad185c9547c49151335da0d6ff7482e9f4390
MayankMehra <mehramayank28@gmail.com>
2026-06-10 15:57:24 +0530

    valid_p

---
+++ FAANG-DSA-Roadmap/00-start-here/optimal/bpalandrome.js
@@ -0,0 +1,20 @@
+const isPalindrome = (s) => {
+  let l = 0, r = s.length - 1;
+
+  const valid = (c) =>
+    (c >= 'a' && c <= 'z') ||
+    (c >= 'A' && c <= 'Z') ||
+    (c >= '0' && c <= '9');
+
+  while (l < r) {
+    while (l < r && !valid(s[l])) l++;
+    while (l < r && !valid(s[r])) r--;
+
+    if (s[l].toLowerCase() !== s[r].toLowerCase()) return false;
+
+    l++;
+    r--;
+  }
+
+  return true;
+};
